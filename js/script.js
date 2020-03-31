52381c48a2e1ea741aafdb7a724ceacf1c183fc6
Alexandra <mirinda-001@list.ru>
2020-03-31 15:23:56 +0300

    create new btn edit,del,save

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ const randomColor = () => {
     return color[i];
 };
 
-add.onclick = (divBtn) => {
+add.onclick = () => {
 	let nameAuthor = document.getElementById('name-author').value;
 	let date = document.getElementById('date-created').value;
 	let toDo = document.getElementById('to-do').value;
@@ -17,11 +17,12 @@ add.onclick = (divBtn) => {
 			nameAuthor: nameAuthor,
 			dateCreated: date,
 			toDo: toDo,
-			color: randomColor(),
+			// color: randomColor(),
 			id: Date.now(),
 			checked: false
 		});
 		addRow('table');
+		// clearyRender();
 	} else alert('Input all filds');
 	console.log('todoList', todoList);
 
@@ -34,24 +35,78 @@ const addRow = () => {
 	let tableRef = document.getElementById('table');
 	let newRow = tableRef.insertRow(-1);
 	newRow.classList = 'row-height';
+	// newRow.style.backgroundColor = randomColor(); //меняет цвет строки
+	const inputNameAuthor = document.querySelector('.name-author');
+	cloneInputNameAuthor = inputNameAuthor.cloneNode(true);
+	cloneInputNameAuthor.setAttribute('disabled', '');
 	let newCell = newRow.insertCell(0);
-	let newText = document.createTextNode(document.getElementById('name-author').value);
-	newCell.appendChild(newText);
+	newCell.appendChild(cloneInputNameAuthor);
 
+	const inputDateCreated = document.querySelector('.date-created');
+	cloneInputDateCreated = inputDateCreated.cloneNode(true);
+	cloneInputDateCreated.setAttribute('disabled', '');
 	let newCellIndOne = newRow.insertCell(1);
-	let newTextIndOne = document.createTextNode(document.getElementById('date-created').value);
-	newCellIndOne.appendChild(newTextIndOne);
+	newCellIndOne.appendChild(cloneInputDateCreated);
 
+
+	const inputToDo = document.querySelector('.to-do');
+	cloneInputToDo = inputToDo.cloneNode(true);
+	cloneInputToDo.setAttribute('disabled', '');
 	let newCellIndTwo = newRow.insertCell(2);
-	let newTextIndTwo = document.createTextNode(document.getElementById('to-do').value);
-	newCellIndTwo.appendChild(newTextIndTwo);
+	newCellIndTwo.appendChild(cloneInputToDo);
 	
 	let newCellIndThree = newRow.insertCell(3);
-	const template = document.querySelector(".div-btn");
-    divBtn = template.cloneNode(true);
-	divBtn.classList.remove('hidden');
-	divBtn.className = 'div-btn';
-	newCellIndThree.appendChild(divBtn);
 
+	let btnEdit = document.createElement('button');
+	btnEdit.type = 'button';
+	btnEdit.innerText = 'Edit';
+	btnEdit.className = 'btn';
+	btnEdit.setAttribute("onclick", "editToDo()");
+
+	let btnDel = document.createElement('button');
+	btnDel.type = 'button';
+	btnDel.innerText = "Del";
+	btnDel.className = 'btn';
+	btnDel.setAttribute("onclick", "delToDo()");
+
+	let btnSave = document.createElement('button');
+	btnSave.type = 'button';
+	btnSave.innerText = "Save";
+	btnSave.className = 'btn-save';
+	btnSave.setAttribute("onclick", "saveToDo()");
+
+	newCellIndThree.appendChild(btnEdit);
+	newCellIndThree.appendChild(btnDel);
+	newCellIndThree.appendChild(btnSave);	
+};
+
+editToDo = (e) => {
+	cloneInputNameAuthor.removeAttribute('disabled', '');
+	cloneInputDateCreated.removeAttribute('disabled', '');
+	cloneInputToDo.removeAttribute('disabled', '');	
 	
 };
+
+saveToDo = () => {
+	nameAuthor = document.getElementById('name-author').value;
+	date = document.getElementById('date-created').value;
+	toDo = document.getElementById('to-do').value;
+
+	nameAuthor.setAttribute('disabled', '');
+	date.setAttribute('disabled', '');
+	toDo.setAttribute('disabled', '');	
+} ;
+
+delToDo = () => {
+	console.log('dddddddddddd');
+
+};
+saveToDo = () => {
+	console.log('ddfewfewfwefwefdddd');
+
+};
+
+// const clearyRender = () => {
+//     const list = document.querySelector(".cleary");
+//     list.innerHTML = '';
+// };
